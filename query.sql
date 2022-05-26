a0692df41d950a606ea66b668d04b1942a4197bb
alexandershevchuk556 <alexandershevchuk556@gmail.com>
2022-05-26 13:56:26 +0300

    ER-diagram

---
+++ query.sql
@@ -0,0 +1,14 @@
+CREATE TABLE sportsman (
+    name VARCHAR(350),
+    email VARCHAR(350),
+    phone VARCHAR(15),
+    birthday DATE,
+    age INT,
+    passport VARCHAR(350),
+    average_competition_place INT,
+    biography TEXT,
+    video_presentation VARCHAR(1000),
+    
+)
+
+SELECT 
